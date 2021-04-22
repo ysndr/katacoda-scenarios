bac45645e7af91219fd0bf2ea8c37bc1dc27a239
Yannik Sander <me@ysndr.de>
2021-04-22 16:40:56 +0200

    Fix challenge commands

--- pijul/prepare-challenge.sh
+++ pijul/prepare-challenge.sh
@@ -19,20 +19,22 @@ $the_cow = <<EOC;
     \\___/
 EOC' >> /root/clippy.cow
 
-alias píjul=cowsay -f /root/clippy.cow "Not a command: pijul"
+alias píjul="cowsay -f /root/clippy.cow 'Not a command: pijul'"
 
 echo "More educational material:\n" >> /challenge/file-b.txt
 echo "https://www.youtube.com/watch?v=dQw4w9WgXcQ" >> /challenge/file-a.txt
 echo "https://www.youtube.com/watch?v=o0ooKVikV3c" >> /challenge/file-c.txt
 
 function change_files_1() {
-    "$(cat file-b.txt && cat file-a.txt) >> file-ab.txt"
+    cat file-b.txt >> ./file-ab.txt
+    cat file-a.txt >> ./file-ab.txt
     rm file-a.txt
     rm file-b.txt
 }
 
 function change_files_a() {
-    "$(cat file-b.txt && cat file-c.txt) >> file-bc.txt"
+    cat file-b.txt >> ./file-bc.txt
+    cat file-c.txt >> ./file-bc.txt
     rm file-b.txt
     rm file-c.txt
 }
