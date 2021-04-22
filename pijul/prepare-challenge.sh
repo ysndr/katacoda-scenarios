1be839af875569988a70ccf8c2c2f8ec90951228
Yannik Sander <me@ysndr.de>
2021-04-22 16:07:29 +0200

    Update pijul fake command

--- pijul/prepare-challenge.sh
+++ pijul/prepare-challenge.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
-alias píjul="echo nonono"
 
-clear
+echo '# Clippy
+#
+# from http://www.reddit.com/r/commandline/comments/2lb5ij/what_is_your_favorite_ascii_art/cltg01p
+#
+$the_cow = <<EOC;
+ $thoughts
+  $thoughts
+     __
+    /  \\
+    |  |
+    @  @
+    |  |
+    || |/
+    || ||
+    |\\_/|
+    \\___/
+EOC' >> /root/clippy.cow
+
+alias píjul=cowsay -f /root/clippy.cow "Not a command:"
+
+clear pi
 
 cd /challenge
