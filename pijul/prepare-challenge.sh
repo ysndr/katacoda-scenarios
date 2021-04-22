a08ce24eb93d4ce31083355aad45997fff434dce
Yannik Sander <me@ysndr.de>
2021-04-22 16:30:50 +0200

    Add challenge commands

--- pijul/prepare-challenge.sh
+++ pijul/prepare-challenge.sh
@@ -19,8 +19,24 @@ $the_cow = <<EOC;
     \\___/
 EOC' >> /root/clippy.cow
 
-alias píjul=cowsay -f /root/clippy.cow "Not a command:"
+alias píjul=cowsay -f /root/clippy.cow "Not a command: pijul"
 
-clear pi
+echo "More educational material:\n" >> /challenge/file-b.txt
+echo "https://www.youtube.com/watch?v=dQw4w9WgXcQ" >> /challenge/file-a.txt
+echo "https://www.youtube.com/watch?v=o0ooKVikV3c" >> /challenge/file-c.txt
+
+function change_files_1() {
+    "$(cat file-b.txt && cat file-a.txt) >> file-ab.txt"
+    rm file-a.txt
+    rm file-b.txt
+}
+
+function change_files_a() {
+    "$(cat file-b.txt && cat file-c.txt) >> file-bc.txt"
+    rm file-b.txt
+    rm file-c.txt
+}
+
+clear
 
 cd /challenge
